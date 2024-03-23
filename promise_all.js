ce7ef8fba8692dc62d053100ce48c289561ed2bb
sergeev1706 <140341803+sergeev1706@users.noreply.github.com>
2024-03-24 00:23:26 +0300

    add new files

--- promise_all.js
+++ promise_all.js
@@ -1,35 +1,45 @@
 
-// ['https://swapi.dev/api/planets/1/', 'https://swapi.dev/api/planets/2/', 'https://swapi.dev/api/planets/3/']
-let species = ['https://swapi.dev/api/species/1/', 'https://swapi.dev/api/species/2/', 'https://swapi.dev/api/species/3/', 'https://swapi.dev/api/species/4/']
+
+// на примере SWAPI
+
+let planets = [
+  'https://swapi.dev/api/planets/1/',
+  'https://swapi.dev/api/planets/2/',
+  'https://swapi.dev/api/planets/3/'
+];
+let species = [
+  'https://swapi.dev/api/species/1/',
+  'https://swapi.dev/api/species/2/',
+  'https://swapi.dev/api/species/3/',
+  'https://swapi.dev/api/species/4/'
+];
 
 async function loadResourse(url) {
-  return await fetch(url).then(res => res.json())
+  return await fetch(url).then(res => res.json());
 }
 
 function getData() {
-  return Promise.all(species.map(url => loadResourse(url))).then((data) => {
-    return data // массив объектов
+  return Promise.all([...planets, ' ', ...species].map(url => url !== ' ' ? loadResourse(url) : url)).then(data => {
+    return data
   })
 }
 
 (async () => {
-  const data = await getdata()
-  data.map(e => console.log(e.name))
-})()
+  const data = await getData();
+  let index = data.indexOf(' ');
 
-function getdata() {
-  return Promise.all([
-    loadResourse('https://swapi.dev/api/species/1/'),
-    loadResourse('https://swapi.dev/api/species/2/'),
-    loadResourse('https://swapi.dev/api/species/3/'),
-  ]).then(([res1, res2, res3]) => {
-    return { res1, res2, res3 }
-  })
-}
+  let pl = data.slice(0, index).map(e => e.name)
+  let sp = data.slice(index + 1).map(e => e.name)
 
-(async () => {
-  const data = await getdata()
-  console.log(data.res1.name)
-})
+  console.log(pl);
+  console.log(sp);
+})();
+
+
+
+
+
+
+
 
 
